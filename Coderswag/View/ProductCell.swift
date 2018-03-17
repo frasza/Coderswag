3755da49a2eddb9b69a1dfe3c7e9cd3b37377f19
Zan Fras <zan.fraas@gmail.com>
2018-03-17 18:05:05 +0100

    create collection view cell

---
+++ Coderswag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  Coderswag
+//
+//  Created by Žan Fras on 17/03/2018.
+//  Copyright © 2018 Žan Fras. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
